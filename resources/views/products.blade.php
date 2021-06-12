75afbd35dfe2554097cf4f61fc8f3bee6cf608a1
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2021-06-13 00:55:47 +0600

    update design

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -9,7 +9,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="grid grid-cols-2">
+            <div class="grid grid-cols-3 gap-4">
                 @livewire('products-table')
                 @livewire('cart-list')
             </div>
